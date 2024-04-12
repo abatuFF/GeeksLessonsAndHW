c8f8c49b9244222b43d28ff2f07a48f1675e5f24
Daniel <nurbekuuludaniel@gmail.com>
2024-04-12 17:37:31 +0600

    upgraded 5 lessons hw

--- lesson_5_hw.py
+++ lesson_5_hw.py
@@ -1,10 +1,9 @@
 def calculate_discount(homework_points, test_points, attendance):
     discount = 0
 
-    # Проверяем условия для скидки
-    if homework_points >= 70-80 and test_points >= 80 and attendance >= 8:
+    if homework_points >= 80 and test_points >= 80 and attendance >= 8:
         discount = 3000
-    elif homework_points >= 80 and test_points == 60 and attendance >= 8:
+    elif homework_points >= 70 and test_points == 60 and attendance >= 8:
         discount = 2500
     elif  homework_points >= 70 and test_points >= 60 and attendance >= 7:
         discount = 2000
@@ -16,18 +15,26 @@ def calculate_discount(homework_points, test_points, attendance):
         discount = 1000
     return discount
 
-payment_amount = 14000
 
-homework_points = int(input("Введите баллы за домашнее задание: "))
-test_points = int(input("Введите баллы за тесты: "))
-attendance = int(input("Введите количество посещений: "))
+while True:
+    agreement = input("Введите 'да' чтобы продолжить, и 'нет' чтобы выйти: ")
+    if agreement.lower() == 'нет':
+        print("закрытие кода")
+        break
+    elif agreement.lower() == 'да':
+        payment_amount = 14000
+        homework_points = int(input("Введите баллы за домашнее задание: "))
+        test_points = int(input("Введите баллы за тесты: "))
+        attendance = int(input("Введите количество посещений: "))
 
 
-discount_amount = calculate_discount(homework_points, test_points, attendance)
-total_price = discount_amount - payment_amount
+        discount_amount = calculate_discount(homework_points, test_points, attendance)
+        total_price = discount_amount - payment_amount
 
 
-if discount_amount > 0:
-    print(f"Вы получаете скидку в размере {discount_amount} рублей. К оплате у вас {total_price} сом")
-else:
-    print("К сожалению, вы не получаете скидки.")
+        if discount_amount > 0:
+            print(f"Вы получаете скидку в размере {discount_amount} рублей. К оплате у вас {total_price} сом")
+        else:
+            print("К сожалению, вы не получаете скидки.")
+    else:
+        print("Пожалуйста введите только 'да' или 'нет' ")
